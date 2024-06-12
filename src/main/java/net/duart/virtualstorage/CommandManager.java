69e7a9b9c27d1242b51a3606124a36c9c6cba1dc
852DuartePls <duartebenitez_001@hotmail.com>
2024-06-11 20:14:16 -0400

    Overall improvement

--- src/main/java/net/duart/virtualstorage/CommandManager.java
+++ src/main/java/net/duart/virtualstorage/CommandManager.java
@@ -1,13 +1,20 @@
 package net.duart.virtualstorage;
 
+import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.command.TabCompleter;
 import org.bukkit.entity.Player;
+import org.bukkit.inventory.InventoryView;
+import org.checkerframework.checker.nullness.qual.NonNull;
+
+import java.util.ArrayList;
+import java.util.List;
 
 
-public class CommandManager implements CommandExecutor {
+public class CommandManager implements CommandExecutor, TabCompleter {
 
     private final VirtualBackpack virtualBackpack;
 
@@ -15,10 +22,9 @@ public class CommandManager implements CommandExecutor {
         this.virtualBackpack = virtualBackpack;
     }
 
-    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+    public boolean onCommand(@NonNull CommandSender sender,@NonNull Command command,@NonNull String label,@NonNull String[] args) {
         if (command.getName().equalsIgnoreCase("backpack")) {
-            if (sender instanceof Player) {
-                Player player = (Player) sender;
+            if (sender instanceof Player player) {
                 for (int i = 1; i <= 999; i++) {
                     if (player.hasPermission("virtualstorages.use." + i)) {
                         virtualBackpack.openBackpack(player);
@@ -33,6 +39,12 @@ public class CommandManager implements CommandExecutor {
             }
         } else if (command.getName().equalsIgnoreCase("vsreload")) {
             if (sender.hasPermission("virtualstorages.admin.reload")) {
+                for (Player player : Bukkit.getOnlinePlayers()) {
+                    InventoryView openInventory = player.getOpenInventory();
+                    if (openInventory.getTitle().contains("Backpack - Page")) {
+                        player.closeInventory();
+                    }
+                }
                 virtualBackpack.updatePermissions();
                 sender.sendMessage(ChatColor.GREEN + "VirtualStorages permissions reloaded.");
             } else {
@@ -41,5 +53,15 @@ public class CommandManager implements CommandExecutor {
             return true;
         }
         return false;
+    }
+
+    @Override
+    public List<String> onTabComplete(@NonNull CommandSender sender, @NonNull Command command, @NonNull String alias, @NonNull String[] args) {
+        List<String> completions = new ArrayList<>();
+        if (args.length == 1) {
+            completions.add("backpack");
+            completions.add("vsreload");
+        }
+        return completions;
     }
 }
--- src/main/java/net/duart/virtualstorage/VirtualStorages.java
+++ src/main/java/net/duart/virtualstorage/VirtualStorages.java
@@ -15,8 +15,11 @@ public final class VirtualStorages extends JavaPlugin {
         getLogger().info("||    by DaveDuart     ||");
         virtualBackpack = new VirtualBackpack(this);
 
-        Objects.requireNonNull(getCommand("backpack")).setExecutor(new CommandManager(virtualBackpack));
-        Objects.requireNonNull(getCommand("vsreload")).setExecutor(new CommandManager(virtualBackpack));
+        CommandManager commandManager = new CommandManager(new VirtualBackpack(this));
+        Objects.requireNonNull(getCommand("backpack")).setExecutor(commandManager);
+        Objects.requireNonNull(getCommand("vsreload")).setExecutor(commandManager);
+        Objects.requireNonNull(getCommand("backpack")).setTabCompleter(commandManager);
+        Objects.requireNonNull(getCommand("vsreload")).setTabCompleter(commandManager);
         getServer().getPluginManager().registerEvents(virtualBackpack, this);
         File dataFolder = getDataFolder();
         if (!dataFolder.exists()) {
@@ -33,6 +36,7 @@ public final class VirtualStorages extends JavaPlugin {
     @Override
     public void onDisable() {
         if (virtualBackpack != null) {
+            virtualBackpack.createBackup();
             virtualBackpack.saveAllBackpacks();
         }
         getLogger().info("VirtualStorages Disabled.");
